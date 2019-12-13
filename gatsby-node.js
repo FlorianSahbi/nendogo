eae03eb58042b7d47a86884d09414f3db9dbe927
FlorianSahbi <florian.sahbi@gmail.com>
2019-12-13 08:32:00 +0100

    ADD : User auth using Auth0

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,3 +66,28 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  if (page.path.match(/^\/account/)) {
+    page.matchPath = "/account/*"
+
+    createPage(page)
+  }
+}
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /auth0-js/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"
 import { ApolloProvider } from 'react-apollo';
 import { client } from './client';
 
+import { checkSession } from '../utils/auth';
+
+const SessionCheck = ({ children }) => {
+    const [loading, stillLoading] = useState(true);
+    useEffect(() => checkSession(() => stillLoading(false)));
+    return loading === false && <>{children}</>
+};
+
 export const wrapRootElement = ({ element }) => (
-    <ApolloProvider client={client}>
-        {element}
-    </ApolloProvider>
+    <SessionCheck>
+        <ApolloProvider client={client}>
+            {element}
+        </ApolloProvider>
+    </SessionCheck>
 )
