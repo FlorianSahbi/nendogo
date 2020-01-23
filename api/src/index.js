42b12779de8213b5061cbaed02d97c28ca2820c0
FlorianSahbi <florian.sahbi@gmail.com>
2020-01-23 10:03:43 +0100

    ADJUST : GraphqlYoga Config

--- api/src/index.js
+++ api/src/index.js
@@ -1,37 +1,43 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
 const Subscription = require('./resolvers/Subscription');
-const path = require("path");
-const { existsSync, mkdirSync } = require("fs");
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Image = require('./resolvers/Image');
 const Nendoroid = require('./resolvers/Nendoroid');
 const Interaction = require('./resolvers/Interaction');
-require('dotenv').config();
-
-existsSync(path.join(__dirname, "../images")) || mkdirSync(path.join(__dirname, "../images"));
 
 const resolvers = {
-    Query,
-    Mutation,
-    Subscription,
-    User,
-    Image,
-    Nendoroid,
-    Interaction,
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Image,
+  Nendoroid,
+  Interaction,
 }
 
 const server = new GraphQLServer({
-    typeDefs: './schema.graphql',
-    resolvers,
-    context: request => {
-        return {
-            ...request,
-            prisma,
-        }
-    },
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+const options = {
+  port: 4000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
+}
+
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+)
--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
 import fetch from 'isomorphic-fetch';
 
-const link = createUploadLink({ uri: "http://localhost:4000" });
+const link = createUploadLink({ uri: "http://localhost:4000/graphql" });
 
 export const client = new ApolloClient({
   link,
